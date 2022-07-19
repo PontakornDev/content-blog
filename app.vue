58993ad27309c4120335efd4469e6d95b9cf7356
Pontakorn32 <Pontakorn322@gmail.com>
2022-07-19 13:44:24 +0700

    saa tuu

--- app.vue
+++ app.vue
@@ -14,6 +14,7 @@ export default defineComponent ({
       titleTemplate: (title) => {
         return `Dev Learning - ${title}`;
       },
+      title: "",
       meta: [
         { name: "keywords", content: "",},
         { name: "description", content: "",},
