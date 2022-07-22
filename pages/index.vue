5dbd98ea8edf3b65a7ad9c40eba4e16e6df652f1
Pontakorn32 <Pontakorn322@gmail.com>
2022-07-23 03:42:25 +0700

    des title main page

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,11 @@
 				>
 					Check Out Our Latest Blog Posts
 				</h1>
+				<h2
+					class="lg:text-2xl text-xl text-center leading-normal font-bold rainbow-text"
+				>
+					(Dev Learning - เป็นเว็บไซต์พัฒนาขึ้นเพื่อแบ่งปันความรู้)
+				</h2>
 			</section>
 			<section class="lg:px-[15%] px-[5%] lg:pt-20 pt-14">
 				<p
