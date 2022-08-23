3bfa90c9539261ac9bee56ae2a4d24f9f6c13b56
C.PTK <pontakorn322@gmail.com>
2022-08-23 15:45:10 +0700

    button share facebook

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,8 +1,8 @@
 <template>
-    <footer class="p-4 mt-8 bg-gray-200 rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+    <footer class="p-4 mt-8 bg-gray-200 rounded-lg shadow md:flex md:items-center md:p-6 dark:bg-gray-800">
     <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© {{currentYear}} <b>Dev Learning</b>. All Rights Reserved.
     </span>
-    <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+    <ul class="flex flex-wrap items-center mt-3 md:ml-5 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
             <a href="https://github.com/PontakornDev" class="mr-4 hover:text-fuchsia-700 md:mr-6">
                 <Icon class="inline" icon="mdi:github" width="30" height="30" /> Github
--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -2,14 +2,21 @@
 	<div>
 		<main>
 			<div 
-				class="lg:space-x-10 px-[5%] lg:px-[10%] pt-5"
+				class="lg:space-x-10 px-[5%] lg:px-[10%] pt-10"
 			>
 				<div class="flex w-full border-b-2 border-gray-400 inline-block">
 					<Icon icon="mdi:calendar-range" width="24" height="24"/>
 					<p v-if="!blog.head.updateAt">เขียนเมื่อวันที่: {{blog.head.createAt}}</p>
 					<p v-else>อัพเดทเมื่อวันที่: {{blog.head.updateAt}}</p>
-					<Icon class="ml-5" icon="mdi:face-man-shimmer" width="24" height="24" />
+					<Icon class="ml-2 md:ml-5" icon="mdi:face-man-shimmer" width="24" height="24" />
 					<p> ผู้เขียน: {{blog.head.writer}}</p>
+					<button 
+					class="bg-blue-500 px-4 py-2 font-semibold text-white inline-flex items-center space-x-2 rounded fixed right-5 bottom-2 z-10"
+					v-on:click="shareFB()"
+					>
+						<svg class="w-5 h-5 fill-current" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/></svg>
+						<span>Facebook</span>
+					</button>
 				</div>
 			</div>
 			<article
@@ -78,6 +85,15 @@
 		return toc;
 	})
 
+	const shareFB = () => {
+		const url = `https://www.devlearning.dev/blog${blog._value._path}`
+		window.open('https://www.facebook.com/sharer/sharer.php?u=' + url,
+        'facebook-share-dialog',
+        'width=800,height=600'
+    );
+    return false;
+	}
+
 	useHead({
 		title: `${blog.value.head.title}`,
 		meta: [
@@ -103,7 +119,7 @@
 			],
 		link: [
 			{rel: "canonical", href: `https://www.devlearning.dev/blog${blog._value._path}`,}
-        ],
+        ]
 	});
 </script>
 
